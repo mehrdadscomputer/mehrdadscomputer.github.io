eb56e6eed2d2b7800f96a215201e9f7f8317027c
Mehrdad <mehrdadscomputer@gmail.com>
2022-06-19 23:58:24 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,122 @@
+const data = {
+    profile: {
+        name: "Mehrdad Salimi",
+        research_area: "I am working on Twitter data. My primary duty is to purify Twitter data by removing bots using machine learning and other automated algorithms. " +
+            "Also, I will try to bring up an easy-to-use web interface for the non-technical community of researchers to help them work with the data mentioned above and get insight from it.",
+        thumbnail: "profile.png",
+        phone: "+358 46 895 9237",
+        email: "salimi [at] cs [do] uef [dot] fi",
+        social: {
+            // email: {
+            //     icon: "gmail.ico",
+            //     title: "Gmail",
+            //     link: "mehrdadscomputer@gmail.com"
+            // },
+            linkedin: {
+                icon: "linkedin.ico",
+                title: "Linkedin",
+                link: "https://www.linkedin.com/in/mehrdad-salimi12/"
+            },
+        }
+    },
+    side: {
+        research_interest: {
+            title: "Research Interests",
+            values: {
+                a: {
+                    title: null,
+                    items: ["Machine learning", "Fraud detection in social media and online platforms",
+                        "Natural language processing",]
+                },
+            }
+        },
+        supervisers: {
+            title: "Supervisors",
+            values: {
+                pasi: {
+                    description: "Prof. Pasi Fränti",
+                    note: "School of Computing, University of Eastern Finland",
+                },
+                mikko: {
+                    description: "Prof. Mikko Laitinen",
+                    note: "Department of Humanities, Foreign Languages ​​and Translation Studies, University of Eastern Finland",
+                }
+            }
+        },
+        address: {
+            title: "Address",
+            values: {
+                university1: {
+                    description: "",
+                    note: "University of Eastern Finland",
+                },
+                university2: {
+                    description: "",
+                    note: "School of Computing",
+                },
+                university3: {
+                    description: "",
+                    note: "Office TB357, Science Park",
+                },
+                university4: {
+                    description: "",
+                    note: "Joensuu, Finland",
+                },
+            }
+        }
+    },
+    body: {
+        work: {
+            title: "Education Records",
+            values: {
+                IUST: {
+                    title: "M.Sc. in Computer Science - Artificial Intelligence from Iran University of Science and Technology (IUST)",
+                    site: "http://iust.ac.ir/en",
+                    icon: "iust.png",
+                    description: null,
+                    achievements: [
+                        "Thesis: Online Marketplaces Comments Fraud Detection Using Neural Networks",
+                    ],
+                    date: {
+                        start: "Oct, 2014",
+                        end: "Sep, 2017",
+                    }
+                },
+                SUT: {
+                    title: "B.Sc. in Computer Engineering - Software Engineering from Shahrood University of Technology",
+                    site: "https://shahroodut.ac.ir/en/",
+                    icon: "shahroodut.jpg",
+                    description: null,
+                    achievements: [
+                        "Final Project: Android ChatBot to simulate human-to-human conversation.",],
+                    date: {
+                        start: "Oct - 2009",
+                        end: "Sep - 2013",
+                    }
+                },
+            }
+        },
+        Group: {
+            title: "Group",
+            values: {
+                dragon: {
+                    title: "Machine Learning Group - School of Computing",
+                    site: "https://cs.uef.fi/ml/",
+                    icon: null,
+                    description: "Machine Learning Group at University of Eastern Finland",
+                    achievements: [
+                        "The main research areas are clustering methods, location-based services, data and web mining, and optimizing health care applications. " +
+                        "Currently there are also activities in TSP, Twitter data analysis, intelligent robotics, algorithmic music composition, and biomedical signal analysis. " +
+                        "Earlier topics have been image and speech processing.",
+                    ],
+                    date: {
+                        start: "June - 2022",
+                        end: "present",
+                    }
+                },
+            }
+        }
+    },
+    jobPosition: "Early Stage Researcher in Computer Science / PhD Student",
+    footer: null
+}
